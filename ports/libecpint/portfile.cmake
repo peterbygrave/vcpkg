2af962e9556b3176d7a98ef3c31186579a4d89d8
Peter Bygrave <bygrave@gmail.com>
2020-03-25 10:05:37 +0000

    Update libecpint to peterbygrave/libecpint to get a tag

--- ports/libecpint/portfile.cmake
+++ ports/libecpint/portfile.cmake
@@ -1,6 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO moritzBens/libecpint
+    REPO peterbygrave/libecpint
+    REF v0.0.0
+    SHA512 e3f30c4a31987af179db419e796bca4297348c9b446b4282c6b96ec51ff5f86f1a02103de283e58da0913eca0f51db47876fd2e1c81e509792841ba2fb712057
     HEAD_REF master
 )
 
