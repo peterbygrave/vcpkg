402f6ceaf09705bb9fbd19a75ed323dc3f5f41fd
Peter Bygrave <bygrave@gmail.com>
2020-03-30 17:31:32 +0100

    Make armadillo dynamic and add [wrapper] option if needed

--- ports/armadillo/portfile.cmake
+++ ports/armadillo/portfile.cmake
@@ -1,7 +1,5 @@
 include(vcpkg_common_functions)
 
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
@@ -18,12 +16,18 @@ file(REMOVE ${SOURCE_PATH}/cmake_aux/Modules/ARMA_FindBLAS.cmake)
 file(REMOVE ${SOURCE_PATH}/cmake_aux/Modules/ARMA_FindLAPACK.cmake)
 file(REMOVE ${SOURCE_PATH}/cmake_aux/Modules/ARMA_FindOpenBLAS.cmake)
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
+    wrapper   ARMA_USE_WRAPPER
+)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     DISABLE_PARALLEL_CONFIGURE
     PREFER_NINJA
     OPTIONS
         -DDETECT_HDF5=false
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_install_cmake()
