0a6332a1a048f1e00dafdef7bb60ee1391828b79
Peter Bygrave <bygrave@gmail.com>
2020-03-30 10:56:34 +0100

    Add patch for linking to libm for libxc

--- ports/libxc/portfile.cmake
+++ ports/libxc/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_gitlab(
     REPO libxc/libxc
     REF 4.3.4
     SHA512 ccb9edd2dbedcef943031ad297da181d25e62b86e0118764cb625c443c3bd447a9b3566c56c3d5ff7fc1b83196c1b2e6fa2050ce50361860e90d692fc1573228
+    PATCHES
+        cmake-policy.patch
 )
 
 vcpkg_configure_cmake(
